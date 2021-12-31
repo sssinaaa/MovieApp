cca1a8f25a84450b07200ed4c96ec1b982efe7ce
Sina <43543230+sssinaaa@users.noreply.github.com>
2021-12-31 16:12:36 +0330

    react navigation setup completed

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+
 import HomeScreen from './src/screens/HomeScreen';
 
 import {Provider} from 'react-redux';
 import store from './src/store/store';
 
+import {NavigationContainer} from '@react-navigation/native';
+
 const App = () => {
   return (
     <Provider store={store}>
-      <View style={{flex: 1}}>
-        <HomeScreen />
-      </View>
+      <NavigationContainer>
+        <View style={{flex: 1}}>
+          <HomeScreen />
+        </View>
+      </NavigationContainer>
     </Provider>
   );
 };
